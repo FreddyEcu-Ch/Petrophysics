7a44cada79f1427d1846a54c5558071cc5d22f0a
fredd <freddypcarrion@gmail.com>
2022-08-23 11:04:49 -0500

    Project updated

--- petro_app.py
+++ petro_app.py
@@ -15,11 +15,23 @@ icon = Image.open("resources/logs.png")
 st.set_page_config(page_title="Well Logs App", page_icon=icon)
 
 # Title of app
+st.title("Well Logs Visualization")
+st.write("This logs contain information about petrophysical features of any well")
 
 # Pages
 with st.sidebar:
     options = option_menu(
         menu_title="Main Menu",
-        options=["Home", "Header Information", "Data Information"],
-        icons=["house", "clipboard-data", "tv", "calculator"],
+        options=["Home", "Data Information", "Logs Visualization"],
+        icons=["house", "clipboard-data", "tv"],
     )
+
+# Options
+if options == "Data Information":
+    # number of file to upload
+    n_wells = int(st.number_input("Enter the well logs files"))
+    files = [
+        st.file_uploader(f"Upload the las file of well {n + 1}") for n in range(n_wells)
+    ]
+
+elif options == "Logs Visualization":
